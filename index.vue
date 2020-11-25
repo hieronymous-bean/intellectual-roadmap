036be9bb511a78f05d1e756600155cb2afe91a52
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-11-25 14:52:53 -0500

    Project layout and program structure

---
+++ index.vue
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  	<head>
+    	<title>ahcountinterface</title>
+ 	 </head>
+  	<body>
+  		
+  		<template>
+			<div id="wrapper">
+			    <div class="content-page">
+			        <div class="content">
+			            <div class="container-fluid">
+			                
+
+
+			            </div>
+			        </div>
+			    </div>
+			</div>
+		</template>
+
+   </body>
+</html>
