38f50462544c8de0c508ec363070262dee186feb
Jeffrey Brown <jeffreybrown@Jeffreys-MacBook-Pro.local>
2021-10-13 21:35:19 -0400

    journey tile started and routing

--- src/components/workspaces/WorkspaceSelector.vue
+++ src/components/workspaces/WorkspaceSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <a @click="workspaceSelected()" class="relative w-10 h-10 rounded-lg mt-4" :class="{ 'shadow-md': selected }" :style="{ 'background-color': workspaceData.color }" href="#">
+  <a @click="workspaceSelected()" class="relative w-10 h-10 rounded-lg mt-4 cursor-pointer" :class="{ 'shadow-md': selected }" :style="{ 'background-color': workspaceData.color }">
     <span v-show="notifications" class="absolute w-3 h-3 rounded-full bg-blue-400 top-0 right-0 -mt-1 -mr-1"></span>
   </a>
 </template>
@@ -11,9 +11,8 @@ export default ({
   }),
   computed: {
     selected: function() {
-      // let currentWorkspace = this.$store.getters['workspace/getCurrentWorkspace'];
-      // return this.workspaceData.id == currentWorkspace.id;
-      return true;
+      let currentWorkspace = this.$store.getters['workspace/getCurrentWorkspace'];
+      return this.workspaceData.id == currentWorkspace.id;
     }
   },
   methods: {
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,22 @@ import { createRouter, createWebHistory } from 'vue-router';
 // application containers //
 import ApplicationContainer from '../containers/ApplicationContainer.vue';
 
+// application views //
+import Dashboard from '../views/Dashboard.vue';
+
 const routes = [
   {
     path: '/',
     component: ApplicationContainer,
-    name: "ApplicationContainer"
+    name: 'ApplicationContainer',
+    redirect: '/app/dashboard',
+    children: [
+      {
+        path: '/app/dashboard',
+        component: Dashboard,
+        name: "Dashboard"
+      }
+    ]
   }
 ]
 
