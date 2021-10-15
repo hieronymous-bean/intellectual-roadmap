7560b2d84f27f73ecbf4b4fcbfb9b09056da5e47
Jeffrey Brown <jeffreybrown@Jeffreys-MacBook-Pro.local>
2021-10-15 14:22:07 -0400

    login flow implemented for google auth

--- api/authentication/googleOAuth.js
+++ api/authentication/googleOAuth.js
@@ -4,28 +4,29 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // import helper functions for database lookups //
 const { findOrCreateUser } = require('../../database/user/index');
 
-// google oauth2.0 strategy //
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  findOrCreateUser((profile), function(err, data) {
-    return done(err, data);
-  })
-}));
-
 module.exports = function(app){
+
+  // google oauth2.0 strategy //
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+    scope: [ 'profile' ]
+  },
+  function(accessToken, refreshToken, profile, done) {
+    findOrCreateUser((profile), function(err, data) {
+      return done(err, data);
+    })
+  }));
+
   app.get('/auth/google', passport.authenticate('google', { 
     scope: ['profile'] 
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/auth/login' }),
-  function(req, res) {
-    console.log(req)
-    res.send('ok')
-  });
+  app.get('/auth/google/callback', passport.authenticate('google', { 
+    successRedirect: '/app/dashboard',
+    failureRedirect: '/auth/login' 
+  }));
 
   passport.serializeUser(function(user, cb) {
     cb(null, user);
--- server.js
+++ server.js
@@ -1,12 +1,88 @@
 'use strict';
 
+require('dotenv').config();
+
 // configure express //
 const express = require('express');
 const app = express();
-require('dotenv').config();
+
+// -------------------------------------- //
+// ---------- AUTHENTICATION ------------ //
+// -------------------------------------- //
+
+
+
+
+const session = require('express-session');
+const passport = require('passport');
+
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+// session middleware //
+app.use(session({
+  secret: "tony",
+  resave: false ,
+  saveUninitialized: true ,
+}));
+
+app.use(passport.initialize()) // init passport on every route call
+app.use(passport.session())    //allow passport to use "express-session"
+
+// define user //
+const authUser = (request, accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}
+
+// use google strategy as the passport authentication strategy //
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
+  passReqToCallback   : true
+}, authUser));
+
+
+passport.serializeUser( (user, done) => { 
+  console.log(`\n--------> Serialize User:`)
+  console.log(user)
+   // The USER object is the "authenticated user" from the done() in authUser function.
+   // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
+
+  done(null, user)
+} )
+
+
+passport.deserializeUser((user, done) => {
+  console.log("\n--------- Deserialized User:")
+  console.log(user)
+  // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
+  // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+
+  done (null, user)
+})
+
+app.get('/auth/google',
+  passport.authenticate('google', { 
+    scope:['email','profile']
+  }
+));
+
+app.get('/auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: 'http://localhost:8080/app/dashboard',
+    failureRedirect: 'http://localhost:8080/auth/login'
+}));
+
+
+// -------------------------------------- //
+// ---------- AUTHENTICATION ------------ //
+// -------------------------------------- //
+
+var logger = require('morgan');
+app.use(logger('dev'));
 
 // cors middleware //
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 
 // body parsing middleware //
@@ -18,7 +94,7 @@ app.use(bodyParser.json());
 require('./database/workspace/index.js')(app);
 
 // load authentication middleware and routes //
-require('./api/authentication/googleOAuth.js')(app);
+// require('./api/authentication/googleOAuth.js')(app);
 
 // connect to mongodb database //
 const mongoose = require('mongoose');
@@ -31,6 +107,7 @@ async function main() {
 };
 
 main().catch(err => console.log(err));
+
 
 // main server init //
 const port = process.env.SERVER_PORT || '3000';
